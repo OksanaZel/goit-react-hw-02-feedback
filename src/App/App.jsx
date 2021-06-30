e5e6f0790e0bf4d831a1b75c3458bc6a8671a7b8
Oksana Zelenevskaya <exoticisl@gmail.com>
2021-06-30 12:55:43 +0300

    Create react app

---
+++ src/App/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Container} from "./App.styles";
+
+function App() {
+  return (
+    <Container>
+      
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/App/App.styles.jsx
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+width: 600px;
+height: 600px;
+background: #FFFFFF;
+box-shadow: 0px 4px 10px rgba(55, 19, 187, 0.25);
+margin: 0 auto;
+`;
